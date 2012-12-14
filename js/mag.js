ee4d2a553fa44a6c8079134ccb6ce0229f707d6d
James Daniel Holby <jamesdholby@gmail.com>
2012-12-13 23:06:59 -0500

    Mitch wanted the sticky header to be fixed with 95% opacity.

--- js/mag.js
+++ js/mag.js
@@ -8,27 +8,6 @@ $(document).ready( function()
 		
 		$( "img" + $(this).attr("href") ).css({"z-index":"1", "opacity":"1"}).siblings("img").css( { "z-index" : "-1", "opacity":"0"});
 		
-		console.log( $(this).attr("href") );
 	});
 	
-	$( document ).on("scroll", function() 
-	{
-		if ( window.pageYOffset > navHeight )
-		{
-			$("nav").css(
-			{ 
-				"position": "fixed", 
-				"background-color": "rgba(255, 255, 255, 0.95",
-				"padding-right": ".5em",
-				"width": "90%",
-				"z-index": "20",
-				"margin-top": "0"
-			});
-		}
-		
-		if ( window.pageYOffset <= navHeight )
-		{
-			$("nav").removeAttr("style");
-		}
-	});
 });
