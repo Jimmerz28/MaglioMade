0e71a737249a177f4a0b662e683377c88ca1d57c
James Daniel Holby <jamesdholby+github@gmail.com>
2012-12-17 02:33:01 -0500

    Page scroll and no updated hash.
    Now the page scrolls like Mitch wanted and the #work section doesn't
    update the hash tag so the page doesn't jump when changing the sections.

--- js/mag.js
+++ js/mag.js
@@ -1,13 +1,44 @@
 $(document).ready( function() 
 {		
 	var navHeight = $("nav").outerHeight(true);
-
+	jQuery.easing.def = "easeOutBack";
+	
 	$("#work section p + div > a").click( function( event, ui ) 
 	{
 		event.preventDefault();
 		
 		$( "img" + $(this).attr("href") ).css({"z-index":"1", "opacity":"1"}).siblings("img").css( { "z-index" : "-1", "opacity":"0"});
 		
+	});
+	
+	$("a:lt(2)").on( "click", function( event, ui )
+	{
+		event.preventDefault();
+		
+		var target = this.hash,
+        $target = $(target);
+ 
+		$('html, body').stop().animate(
+		{
+			'scrollTop': $target.offset().top - 65
+		}, 1000 );
+	});
+	
+	$("#work section > a:lt(4)").on("click", function( event, ui )
+	{
+		event.preventDefault();
+		
+		$($(event.target).parents("section")[0]).siblings().children("div").css(
+		{
+			"opacity": "0",
+			"z-index": "-1" 
+		});
+		
+		$($(event.target).parents("section")[0]).children("div").css(
+		{
+			"opacity": "1",
+			"z-index": "2"
+		});
 	});
 	
 });
