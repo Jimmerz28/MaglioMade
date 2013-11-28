9c5c35ef9287ab6faf5342b45a15b0447ead1bc6
James Daniel Holby <jamesdholby@gmail.com>
2013-11-28 20:15:47 +0100

    HTML replaced with Mitchell's own words and the indicators are removed and replaced with the JavaScript that will generate them automatically.

--- js/brains.js
+++ js/brains.js
@@ -1,20 +1,101 @@
-// TODO: Create a function to encase each string within the introBlurb.json array into a <p/>
+function ResumeReel(container)
+{
+	var self = container;
+	
+	self.lists = $(container).find(".container").children("div");
+	self.container = container.getElementsByClassName("container");
+		
+	self.activeIndex = $(self.lists).index(".activeObject");
+	self.size = $(self.lists).size();
+	
+	// Begin Probable Function
+	var indicatorContainer = document.createElement("div");
+	indicatorContainer.className = "indicators";
+	var svgs = "";
+	
+	for ( var i = 0; i < self.size; i++ )
+	{
+		if ( i === 0 )
+		{
+			svgs += '<svg class="activeIndicator"><rect width="10" height="10" fill="white"></rect></svg>';
+		}
+		
+		else
+		{
+			svgs += '<svg><rect width="10" height="10" fill="white"></rect></svg>';
+		}
+	}
+	
+	indicatorContainer.innerHTML = svgs;
+	
+	var frag = document.createDocumentFragment();
+	frag.appendChild(indicatorContainer);
+	
+	container.appendChild(frag);
+	
+	// End probable function
+	
+	self.indicators = container.getElementsByClassName("indicators");
+	self.indicators = self.indicators[0].children;
+		
+	$(self).find(".rightArrow, .leftArrow").on("click", function()
+	{
+		var offset = ($(this).attr("class") === "rightArrow") ? 1 : -1;
+		var proposedOffset = self.activeIndex + offset;
+		
+		if ( proposedOffset < 0 )
+		{
+			self.activeIndex = 0;
+			return;
+		}
+		
+		else if ( proposedOffset > (self.size - 1) )
+		{
+			self.activeIndex = self.size - 1;
+			return;
+		}
+		
+		else
+		{
+			self.activeIndex = proposedOffset;
+		}
+		
+		$(self.lists).eq(proposedOffset - offset).fadeOut("fast", function()
+		{
+			$(self.lists).removeClass();
+			$(self.lists).eq(self.activeIndex).fadeIn("fast");
+			// TODO Change the indicator
+			
+			for ( var i = 0; i < self.indicators.length; i++ )
+			{
+				if ( i === self.activeIndex )
+				{
+					self.indicators[i].setAttribute("class", "activeIndicator");
+				}
+				
+				else
+				{
+					self.indicators[i].setAttribute("class", "");
+				}	
+			}
+		});
+	});
+}
 
 $(document).ready(function()
 {
 	$("#initial + div").hide();
+	
+	var reel = new ResumeReel(document.getElementById("resumeCopy"));
 });
 
 var pastPoint = false;
 
 $(document).on("scroll", function()
-{
-	
+{	
 	if ( $(this).scrollTop() > 100 && pastPoint === false )
 	{
 		$("header").addClass("fadeItBack");
-		// $("#initial + div").removeClass("hideMe");
-		// $("#initial").addClass("hideMe");
 		
 		$("#initial").fadeOut(200, function()
 		{
@@ -27,8 +108,6 @@ $(document).on("scroll", function()
 	if ( $(this).scrollTop() < 100 && pastPoint === true )
 	{
 		$("header").removeClass();
-		// $("#initial + div").addClass("hideMe");
-		// $("#initial").removeClass("hideMe");
 		
 		$("#initial + div").fadeOut(200, function()
 		{
