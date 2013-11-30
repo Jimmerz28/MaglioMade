de0e3e56f1637b3d2298b73069811bda91353d06
James Daniel Holby <jamesdholby@gmail.com>
2013-11-30 17:38:05 +0100

    HTML validation, image compression and JS minification done.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,23 +3,107 @@ module.exports = function(grunt)
 	grunt.initConfig(
 	{
 		pkg: grunt.file.readJSON("package.json"),
-		validation: 
-		{ 
-			// Grunt w3c validation plugin
-			options: 
+		uglify:
+		{
+			options:
 			{
-				reset: grunt.option('reset') || false,
-				stoponerror:false,
-				relaxerror: ["Bad value X-UA-Compatible for attribute http-equiv on element meta.","Element title must not be empty."]
+				"report": "gzip"
 			},
-			files: 
+			my_target:
 			{
+				files:
+				{
+					"src/js/brains.min.js": ["js/brains.js"]
+				}
+			}
+		},
+		imagemin:
+		{
+			dynamic:
+			{
+				options:
+				{
+					optimizationLevel: 7,
+					
+				},
+				files:
+				[{
+					expand: true,
+					cwd: "images/",
+					src: ["**/*.{png,jpg,gif}"],
+					dest: "src/images/"
+				}]
+			}
+		},
+		svgmin:
+		{
+			options:
+			{
+				plugins:
+				[{
+					removeViewBox: true,
+					removeUselessStrokeAndFill: true,
+					removeEmptyAttrs: true
+				}]
+			},
+			dist:
+			{
+				files:
+				[{
+					expand: true,
+					cwd: "images/",
+					src: ["**/*.svg"],
+					dest: "src/images/",
+					ext: "min.svg"
+				}]
+			}
+		},
+		sass: 
+		{
+			dist: 
+			{
+				options: 
+				{
+					includePaths: ['node_modules/node-bourbon/assets/stylesheets/'],
+					outputStyle: 'compressed'
+				},
+				files: 
+				{
+					'src/css/mm.css': 'sass/mm.scss'
+				}
+			}
+		},
+		copy:
+		{
+			main:
+			{
+				files:
+				[
+					{expand: true, src:["fonts/*"], dest: "src/fonts/"},
+					{src: ["index.html"], dest: "src/"}
+				]
+			}
+		},
+		htmlhint:
+		{
+			html1:
+			{
+				options:
+				{
+					"tag-pair": true
+				},
 				src: ["index.html"]
 			}
 		}
 	});	
 	
-	grunt.loadNpmTasks("grunt-html-validation");
+	grunt.loadNpmTasks('grunt-htmlhint');
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-imagemin");
+	grunt.loadNpmTasks("grunt-svgmin");
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	
-	grunt.registerTask("default", ["validation"]);
+	grunt.registerTask("default", ["svgmin","uglify","imagemin","sass","htmlhint","copy"]);
+
 };
