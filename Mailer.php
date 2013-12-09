8bc3aa41c4942b5a66ed4d4a1ad9ee794782f04a
James Daniel Holby <jamesdholby@gmail.com>
2013-12-09 20:03:43 +0100

    Finish up the Mailer class with magic get/set and send methods.

--- Mailer.php
+++ Mailer.php
@@ -5,21 +5,92 @@
 	
 	class Mailer
 	{
-		private $_payload;
-		private $_killjoys;
-		private $_sender;
-		private $_message;
-		private $_subject;
+		private $payload;
+		private $killjoys;
+		private $message;
+		private $subject;
 		
-		function __construct($payload, $killjoys)
+		function __construct($mightLoad, $mightKill)
 		{	
-			if ( !json_decode($payload) || !json_decode($killjoys) )
+			if ( !json_decode($mightLoad) || !json_decode($mightKill) )
 			{
-				throw new InvalidArgumentException("Must be a valid JSON string");
+				throw new InvalidArgumentException("Values must be valid JSON strings!");
 			}
 			
-			$this->_payload = $payload;
-			$this->_killjoys = $killjoys;
+			$possible = json_decode($mightLoad, true);
 			
+			if ( empty($possible) )
+			{
+				throw new InvalidArgumentException("Payload cannot be empty!");
+			}
+			
+			$this->payload = json_decode($mightLoad);
+			$this->killjoys = json_decode($mightKill);
+		}
+		
+		public function __get($property) 
+		{
+			if (property_exists($this, $property)) 
+			{
+				return $this->$property;
+			}
+		}
+		
+		public function __set($property, $value) 
+		{
+			if (property_exists($this, $property)) 
+			{
+				$this->$property = $value;
+			}
+		}
+		
+		private function isBotFieldPopulated()
+		{	
+			$tripWire = false;
+			
+			foreach( $this->killjoys->failOn as $killjoy )
+			{
+				if ( array_key_exists($killjoy, $this->payload) )
+				{
+					if ( !empty($this->payload->{$killjoy}) )
+					{
+						$tripWire = true;	
+					}
+				}
+			}
+			
+			return $tripWire;
+		}
+		
+		public function send($sender, $headers)
+		{
+			$status = '{"status": 49}';
+			
+			if ( !isset($this->message) )
+			{
+				throw new InvalidArgumentException("Message cannot be empty");
+			}
+			
+			if ( !isset($this->subject) )
+			{
+				throw new InvalidArgumentException("Subject cannot be empty!");
+			}
+			
+			if ( !$this->isBotFieldPopulated() )
+			{
+				if ( mail($sender, $this->subject, $this->message, $headers) )
+				{
+					echo "Your request has been submitted!";
+					$status = '{"status": 61}';
+				}
+				
+				else
+				{
+					echo "There was an error sending your request. Please try again.";
+					$status = '{"status": 95}';
+				}
+			}
+			
+			return $status;
 		}
 	}
